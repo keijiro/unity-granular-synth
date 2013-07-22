852b6bd3887b9972958bdd5e7bfe2fad459e5cf9
Keijiro Takahashi <keijiro@gmail.com>
2013-07-23 08:12:35 +0900

    Added GUI.

--- Assets/GranularSynth.js
+++ Assets/GranularSynth.js
@@ -2,35 +2,69 @@
 
 var clip : AudioClip;
 
-var step = 2;
-var grain = 100;
-var grainStep = 20;
+var playbackSpeed = 1;
+var grainSize = 1000;
+var grainStep = 1;
 
-private var tick = 0;
+var guiPlaybackSpeed = 1.0;
+var guiGrainSize = 1000.0;
+var guiGrainStep = 1.0;
+
+private var sampleLength : int;
 private var samples : float[];
+
+private var position = 0;
 private var interval = 0;
 
 function Awake() {
+	sampleLength = clip.samples;
 	samples = new float[clip.samples * clip.channels];
 	clip.GetData(samples, 0);
 }
 
+function OnGUI() {
+	GUILayout.BeginArea(Rect(16, 16, Screen.width - 32, Screen.height - 32));
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Playback Speed: " + playbackSpeed);
+	guiPlaybackSpeed = GUILayout.HorizontalSlider(guiPlaybackSpeed, -4.0, 4.0);
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Grain Size: " + grainSize);
+	guiGrainSize = GUILayout.HorizontalSlider(guiGrainSize, 2.0, 10000.0);
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Grain Step: " + grainStep);
+	guiGrainStep = GUILayout.HorizontalSlider(guiGrainStep, -3000.0, 3000.0);
+	GUILayout.FlexibleSpace();
+	if (GUILayout.Button("RANDOMIZE!")) {
+		guiPlaybackSpeed = Random.Range(-2.0, 2.0);
+		guiGrainSize = Random.Range(200.0, 1000.0);
+		guiGrainStep = Random.Range(-1500.0, 1500.0);
+	}
+	GUILayout.FlexibleSpace();
+	GUILayout.EndArea();
+	
+	playbackSpeed = Mathf.RoundToInt(guiPlaybackSpeed);
+	if (playbackSpeed == 0) playbackSpeed = 1;
+	grainSize = Mathf.RoundToInt(guiGrainSize);
+	grainStep = Mathf.RoundToInt(guiGrainStep);
+}
+
 function OnAudioFilterRead(data : float[], channels : int) {
     for (var i = 0; i < data.Length; i += 2) {
-        data[i] = samples[tick];
-        data[i + 1] = samples[tick + 1];
-        interval--;
-        if (interval <= 0) {
-	        interval = grain;
-	        tick += 2 * grainStep;
+        data[i] = samples[position * 2];
+        data[i + 1] = samples[position * 2 + 1];
+        
+        if (--interval <= 0) {
+	        interval = grainSize;
+	        position += grainStep;
         } else {
-	        tick += 2 * step;
+        	position += playbackSpeed;
         }
-        while (tick >= samples.Length) {
-        	tick -= samples.Length;
+        
+        while (position >= sampleLength) {
+        	position -= sampleLength;
         }
-        while (tick < 0) {
-        	tick += samples.Length;
+        while (position < 0) {
+        	position += sampleLength;
         }
     }
 }
